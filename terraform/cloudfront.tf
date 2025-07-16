269701d2a50e2daba44e7e5fd80ba9a77f15862d
lucas-bruzzone <lucas.rbruzzone@gmail.com>
2025-07-16 09:30:56 -0300

    fix: reduce maxttl

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -48,8 +48,8 @@ resource "aws_cloudfront_distribution" "website" {
     }
 
     min_ttl     = 0
-    default_ttl = 3600
-    max_ttl     = 86400
+    default_ttl = 0
+    max_ttl     = 180
   }
 
   # Comportamento para API (condicional)
